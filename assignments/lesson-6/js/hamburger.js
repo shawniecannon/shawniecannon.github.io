5027f3f4040577e09438ec65d65f21ed955f6649
shawniecannon <cannonsh@byui.edu>
2019-05-31 01:33:25 -0600

    Another day

--- assignments/lesson-6/js/hamburger.js
+++ assignments/lesson-6/js/hamburger.js
@@ -1,3 +1,3 @@
 function toggleMenu() {
-    document.getElementByClassName("point")[0].classlist.toggle('responsive');
+    document.getElementByClassName('point')[0].classlist.toggle('responsive');
 }
