425f7559bf8957870084453c529ab595b072494a
shawniecannon <cannonsh@byui.edu>
2019-06-24 19:47:18 -0600

    json test 1

---
+++ assignments/lesson-9/js/windchill.js
@@ -0,0 +1,6 @@
+var t = parseFloat(document.getElementById('temp').innerHTML);
+var s = parseFloat(document.getElementById('wind').innerHTML);
+
+var f = 35.74 + (0.6215 * t) - (35.75 * (Math.pow(s, 0.16))) + ((0.4275 * t) * (Math.pow(s, 0.16)));
+
+document.getElementById("chill").innerHTML = f.toFixed(1) + "&deg;F";
